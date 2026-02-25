a150ad4d0baa64f985a24afefb22fb678884baf6
Praveen Adapa <parveenadap425@gmail.com>
2026-02-25 14:27:16 +0530

    feat:runtime cache and pwa feature

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "http-cache",
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 24 * 60 * 60,
+        },
+      },
+    },
+  ],
 })
 
 module.exports = withPWA({
-  reactStrictMode: true
+  reactStrictMode: true,
 })
